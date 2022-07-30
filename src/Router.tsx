22e5d94251f99532fb63159928664f46dd1d9efc
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-07-30 19:10:28 -0300

    feat(Create router component): :sparkles:

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { DefaultScreen } from './screens/default'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultScreen />}>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/cart" element={<Cart />} /> */}
+        {/* <Route path="/delivery" element={<Delivery />} /> */}
+      </Route>
+    </Routes>
+  )
+}
