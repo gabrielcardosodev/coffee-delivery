6b4f16c224619d980dd0441731a3a6f7fbf52b09
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-09-24 07:59:15 -0300

    feat(Create cart icon to show quantity of items and add a link to homepage): :sparkles:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import { MapPin, ShoppingCart } from 'phosphor-react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/logo.png'
+import { CoffeeContext } from '../../contexts/CoffeeContext'
 
 export function Header() {
+  const { cart } = useContext(CoffeeContext)
   return (
-    <header className="flex w-full items-center justify-between py-8">
-      <img src={logoImg} alt="" />
+    <header className="sticky top-0 z-50 flex w-full items-center justify-between bg-background-color py-8">
+      <Link to={'/'}>
+        <img src={logoImg} alt="" />
+      </Link>
 
       <div className="flex gap-3">
         <span className="flex items-center gap-1 rounded-md bg-purple-light p-2 text-sm text-purple-dark">
@@ -18,7 +23,25 @@ export function Header() {
           to={'/cart'}
           className="rounded-md bg-yellow-light p-2 transition-colors hover:bg-yellow-primary"
         >
-          <ShoppingCart className="text-yellow-dark" size={22} weight="fill" />
+          {cart && cart.length > 0 ? (
+            <div className="relative">
+              <ShoppingCart
+                className="text-yellow-dark"
+                size={22}
+                weight="fill"
+              />
+
+              <span className="absolute -top-4 -right-4 flex h-5 w-5 items-center justify-center rounded-full bg-yellow-dark text-xs font-bold text-base-card">
+                {cart.length}
+              </span>
+            </div>
+          ) : (
+            <ShoppingCart
+              className="text-yellow-dark"
+              size={22}
+              weight="fill"
+            />
+          )}
         </Link>
       </div>
     </header>
