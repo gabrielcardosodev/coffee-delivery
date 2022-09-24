a0cf632b969640731cd4d0c8cdf7a8d0545dfeeb
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-09-24 08:00:43 -0300

    feat(Add a max width to form and finish styles): :sparkles:

--- src/pages/Cart/components/CartForm/index.tsx
+++ src/pages/Cart/components/CartForm/index.tsx
@@ -6,10 +6,12 @@ import {
   Money,
 } from 'phosphor-react'
 
+import { CartItems } from '../CartItems'
+
 export function CartForm() {
   return (
     <form className="mt-10 flex gap-8" action="">
-      <div className="flex flex-col">
+      <div className="flex w-full max-w-[640px] flex-col">
         <strong className="font-baloo text-lg text-base-subtitle">
           Complete seu pedido
         </strong>
@@ -26,56 +28,56 @@ export function CartForm() {
             </div>
           </div>
 
-          <div className="mt-11">
+          <div className="mt-11 flex flex-col gap-4">
             <input
               type="text"
-              className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
+              className="w-[200px] rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
               placeholder="CEP"
             />
 
             <input
               type="text"
               className="w-full rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-              placeholder="CEP"
+              placeholder="Rua"
             />
 
             <div className="flex w-full gap-3">
               <input
                 type="text"
                 className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-                placeholder="CEP"
+                placeholder="Número"
               />
 
               <input
                 type="text"
-                className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-                placeholder="CEP"
-              />
-
-              <input
-                type="text"
-                className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-                placeholder="CEP"
+                className="w-full rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
+                placeholder="Complemento"
               />
             </div>
 
             <div className="flex w-full gap-3">
               <input
                 type="text"
-                className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-                placeholder="CEP"
+                className="w-full rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
+                placeholder="Bairro"
               />
 
               <input
                 type="text"
-                className="rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
-                placeholder="CEP"
+                className="w-full rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
+                placeholder="Cidade"
+              />
+
+              <input
+                type="text"
+                className="w-full rounded border border-base-button bg-base-input p-3 text-base-text outline-none transition-all placeholder:text-base-label focus:border-yellow-dark"
+                placeholder="UF"
               />
             </div>
           </div>
         </div>
 
-        <div className="mt-4 rounded-md bg-base-card p-10 text-base-subtitle">
+        <div className="mt-4 mb-4 rounded-md bg-base-card p-10 text-base-subtitle">
           <div className="flex gap-2">
             <CurrencyDollar className="text-purple-primary" size={22} />
 
@@ -89,17 +91,17 @@ export function CartForm() {
           </div>
 
           <div className="mt-8 flex w-full gap-3">
-            <button className="flex items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
+            <button className="flex w-full items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
               <CreditCard className="text-purple-primary" size={16} />
               Cartão de Crédito
             </button>
 
-            <button className="flex items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
+            <button className="flex w-full items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
               <Bank className="text-purple-primary" size={16} />
               Cartão de Débito
             </button>
 
-            <button className="flex items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
+            <button className="flex w-full items-center gap-3 rounded-md bg-base-button p-4 text-xs uppercase text-base-text transition-colors hover:bg-base-hover">
               <Money className="text-purple-primary" size={16} />
               Dinheiro
             </button>
@@ -107,34 +109,7 @@ export function CartForm() {
         </div>
       </div>
 
-      <div className="w-full">
-        <strong className="font-baloo text-lg text-base-subtitle">
-          Cafés selecionados
-        </strong>
-
-        <div className="mt-4 rounded-[6px_44px] bg-base-card p-10">
-          <div className="flex flex-col gap-4">
-            <div className="flex w-full justify-between text-sm text-base-text">
-              <span>Total de itens</span>
-              <span className="text-base">R$ 29,70</span>
-            </div>
-
-            <div className="flex w-full justify-between text-sm text-base-text">
-              <span>Entrega</span>
-              <span className="text-base">R$ 3,50</span>
-            </div>
-
-            <div className="flex w-full justify-between text-xl font-bold text-base-subtitle">
-              <span>Total</span>
-              <span>R$ 33,20</span>
-            </div>
-
-            <button className="mt-[6px] w-full rounded-md bg-yellow-primary p-[8px_12px] text-sm font-bold uppercase text-white transition-colors hover:bg-yellow-dark">
-              confirmar pedido
-            </button>
-          </div>
-        </div>
-      </div>
+      <CartItems />
     </form>
   )
 }
