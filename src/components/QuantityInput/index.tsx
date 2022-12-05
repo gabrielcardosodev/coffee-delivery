b559fe78025de66eb7fad2bfb985e3633164670d
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-12-05 11:00:14 -0300

    feat(Add posibility to control the size of the input): :sparkles:

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -1,37 +1,46 @@
-import { Minus, Plus } from 'phosphor-react'
+import { Minus, Plus } from "phosphor-react";
+import classNames from "classnames";
 
 interface QuantityInputProps {
-  handleIncreaseQuantityByOne: () => void
-  handleDecreaseQuantityByOne: () => void
-  quantity: number
+  onIncrease: () => void;
+  onDecrease: () => void;
+  quantity: number;
+  size: "large" | "small";
 }
 
 export function QuantityInput({
-  handleDecreaseQuantityByOne,
-  handleIncreaseQuantityByOne,
+  onIncrease,
+  onDecrease,
   quantity,
+  size,
 }: QuantityInputProps) {
   return (
-    <div className="flex h-fit w-[72px] rounded-md bg-base-button p-2">
+    <div
+      className={classNames("flex w-[72px] rounded-md bg-base-button p-2", {
+        "h-[38]": size === "large",
+        "mt-2 h-8": size === "small",
+      })}
+    >
       <button
         type="button"
-        className="text-purple-primary hover:text-purple-dark"
-        onClick={handleDecreaseQuantityByOne}
+        className="text-purple-primary hover:text-purple-dark disabled:opacity-40"
+        onClick={onDecrease}
+        disabled={quantity <= 1}
       >
         <Minus weight="bold" size={14} />
       </button>
 
-      <span className="w-full border-none bg-transparent text-center text-base-title outline-none">
-        {quantity ?? 0}
+      <span className="flex w-full items-center justify-center border-none bg-transparent text-center align-middle text-base-title outline-none">
+        {quantity ?? 1}
       </span>
 
       <button
         type="button"
         className="text-purple-primary hover:text-purple-dark"
-        onClick={handleIncreaseQuantityByOne}
+        onClick={onIncrease}
       >
         <Plus weight="bold" size={14} />
       </button>
     </div>
-  )
+  );
 }
